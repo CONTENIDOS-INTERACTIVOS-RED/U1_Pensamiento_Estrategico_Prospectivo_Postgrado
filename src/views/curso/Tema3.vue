670f57107c061639e26eaa4a4b97171218334c90
FRANCISCO JOSE LIZCANO REYES <48061264+flizcano@users.noreply.github.com>
2024-12-08 14:35:19 -0500

    Initial commit

---
+++ src/views/curso/Tema3.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(:subTitulo="'NOMBRE TEMA 3'")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+</template>
+
+<script>
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema3',
+  components: {
+    BannerInterno,
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+}
+</script>
+
+<style lang="sass">
+.material-complementario
+  padding-top: 3rem
+  border-top: 9px solid $color-sistema-e
+</style>
